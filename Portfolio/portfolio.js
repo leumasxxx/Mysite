7a1a6c4037ac920c5e564ec70c6c68150d74a6fe
Alabi Samuel <110169533+leumasxxx@users.noreply.github.com>
2023-03-13 17:51:49 +0100

    Add files via upload

---
+++ Portfolio/portfolio.js
@@ -0,0 +1,61 @@
+const services = document.querySelectorAll(".app");
+const appearOptions = {
+    root: null,
+    threshold: 0.5,
+    rootMargin: "-100px"
+
+}
+
+const scrollAppear = new IntersectionObserver(function (entries, scrollAppear) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("appear")
+            scrollAppear.unobserve(entry.target);
+        } else {
+            return;
+        }
+    }
+
+    )
+
+}, appearOptions);
+
+
+services.forEach(service => {
+    scrollAppear.observe(service)
+}
+
+);
+
+const sliders = document.querySelectorAll("#sliders")
+const slideOptions = {
+    root: null,
+    threshold: 0,
+    rootMargin: "-200px"
+
+}
+
+const slideAppear = new IntersectionObserver(function (entries, scrollAppear) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("appear")
+            slideAppear.unobserve(entry.target);
+        } else {
+            return;
+        }
+    }
+
+    )
+
+}, slideOptions);
+
+sliders.forEach(
+    slider=>{
+        slideAppear.observe(slider)
+    }
+);
+
+
+
+
+
